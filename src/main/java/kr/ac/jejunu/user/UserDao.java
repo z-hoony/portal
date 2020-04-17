5cb4a0c0698d4a1e54a22a8461ff66df769b395e
hoony <hoony@hoony.org>
2020-04-17 11:53:10 +0900

    사용자 추가 기능

--- src/main/java/kr/ac/jejunu/user/UserDao.java
+++ src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -4,9 +4,7 @@ import java.sql.*;
 
 public class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/jeju?serverTimezone=Asia/Seoul",
-                "jeju", "jejupw");
+        Connection connection = getConnection();
 
         // prepared는 캐시되어서 재사용하는 경우에 빨라짐
         PreparedStatement preparedStatement =
@@ -26,5 +24,29 @@ public class UserDao {
         connection.close();
 
         return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        // prepared는 캐시되어서 재사용하는 경우에 빨라짐
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        user.setId(resultSet.getInt(1));
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mariadb://localhost/jeju?serverTimezone=Asia/Seoul",
+                "jeju", "jejupw");
     }
 }
--- src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -5,19 +5,35 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+    String password = "1234";
+    String name = "jade";
+
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Integer id = 1;
-        String password = "1234";
-        String name = "jade";
 
         UserDao userDao = new UserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        assertThat(user.getId(), greaterThan(0));
+
+        User insertedUser = userDao.get(user.getId());
+        assertThat(insertedUser.getName(), is(name));
+        assertThat(insertedUser.getPassword(), is(password));
     }
 }
