2e0559197259346ad3bdbb9cf8861bfaba0e1a4b
hoony <hoony@hoony.org>
2020-05-29 13:00:34 +0900

    포털서비스 개발론9-3

--- src/main/java/kr/ac/jejunu/user/SimpleController.java
+++ src/main/java/kr/ac/jejunu/user/SimpleController.java
@@ -14,7 +14,7 @@ public class SimpleController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         User user = userDao.get(Integer.valueOf(request.getParameter("id")));
-        ModelAndView modelAndView = new ModelAndView();
+        ModelAndView modelAndView = new ModelAndView("user");
         modelAndView.addObject("user", user);
         return modelAndView;
     }
