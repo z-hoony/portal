293312b7c082de7edf65cfe8d70242366f7e5ddb
hoony <hoony@hoony.org>
2020-06-05 16:22:45 +0900

    포털서비스 개발론10-1

--- src/main/java/kr/ac/jejunu/user/UserController.java
+++ src/main/java/kr/ac/jejunu/user/UserController.java
@@ -2,17 +2,31 @@ package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
-    
+
     @RequestMapping("/user")
     public User getUser(@RequestParam("id") Integer id) {
         return userDao.get(id);
+    }
+
+    @RequestMapping("/exception")
+    public void exception() {
+        throw new RuntimeException("어이쿠!");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("e", e);
+        return modelAndView;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jade
+  Date: 2020/06/05
+  Time: 4:17 오후
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<h1>
+    Error : ${e.message}
+</h1>
+</body>
+</html>
