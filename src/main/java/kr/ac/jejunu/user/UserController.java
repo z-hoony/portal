52b095ebdbe517cfffdd70c4d2df7c8f93f3061c
hoony <hoony@hoony.org>
2020-06-05 17:13:04 +0900

    포털서비스 개발론10-2

--- src/main/java/kr/ac/jejunu/user/UserController.java
+++ src/main/java/kr/ac/jejunu/user/UserController.java
@@ -4,8 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 @RequestMapping
@@ -23,10 +28,29 @@ public class UserController {
         throw new RuntimeException("어이쿠!");
     }
 
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public void upload() {
+
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file")MultipartFile file, HttpServletRequest request) throws IOException {
+        File path = new File(request.getServletContext().getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url", "/images/" + file.getOriginalFilename());
+        return modelAndView;
+    }
+
     @ExceptionHandler(Exception.class)
     public ModelAndView error(Exception e) {
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("e", e);
+        System.out.println(e);
         return modelAndView;
     }
 }
